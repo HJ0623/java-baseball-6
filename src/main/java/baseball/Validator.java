6f8506667b6a448a9b154843673d89fa9b439caa
hyeongjin <rlagudwls277@naver.com>
2024-08-05 19:32:17 +0900

    FEAT: 게임의 화면을 출력하는 기능

---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Validator {
+    public static void validateInput(String input) {
+        if (input.length() != 3) {     // 길이가 3이 아닐떄
+            throw new IllegalArgumentException();
+        }
+        Set<Character> uniqueDigits = new HashSet<>();
+        for (char c : input.toCharArray()) { //각문자가 숫자인지 0인지 확인
+            if (!Character.isDigit(c) || c == '0') {
+                throw new IllegalArgumentException();
+            }
+            uniqueDigits.add(c);
+        }
+        if (uniqueDigits.size() != 3) {  // 고유한 숫자의 개수확인
+            throw new IllegalArgumentException();
+        }
+    }
+}
