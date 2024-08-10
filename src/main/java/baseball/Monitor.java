15ec2859d760784ac8e124060cd3ab98b5a2494d
hyeongjin <rlagudwls277@naver.com>
2024-08-10 21:19:13 +0900

    Refactor:영어문장 한글로 변경

--- src/main/java/baseball/Monitor.java
+++ src/main/java/baseball/Monitor.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 class Monitor {
     private static final Scanner scanner = new Scanner(System.in);
     private static int[] computerNumber;
+    private static final String RESTART= "1";
+    private static final String EXIT="2";
 
     public static void startGame() {
-        System.out.println("Start NumberBaseBall Game.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         boolean continuePlaying = true;
         while (continuePlaying) {
             computerNumber = CreateNumber.createRandomNumber();
             boolean isGameWon = false;
             while (!isGameWon) {
                 try {
-                    System.out.print("Enter Number ");
+                    System.out.print("숫자를 입력해주세요: ");
                     String userInput = scanner.nextLine();
                     Validator.validateInput(userInput);
                     int[] userNumber = parseInput(userInput);
-                    String result = CountBall.getResult(computerNumber, userNumber);
+                    String result = Count.getResult(computerNumber, userNumber);
                     System.out.println(result);
-                    if (result.equals("3strike")) {
-                        System.out.println("collect 3 number! End game");
+                    if (result.equals("3스트라이크")) {
+                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                         isGameWon = true;
                     }
                 } catch (IllegalArgumentException e) {
@@ -41,15 +44,15 @@ class Monitor {
     }
 
     private static boolean reStart() {    //게임 재시작 메서드
-        System.out.println("restart");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."); // 설명이 부족했음
         while(true) {
             String choice = scanner.nextLine();
-            if (choice.equals("1")) {
+            if (choice.equals(RESTART)) {
                 return true;
-            } else if (choice.equals("2")) {
+            } else if (choice.equals(EXIT)) {
                 return false;
             } else {
-                System.out.println("wrong input please input 1 or 2");
+                System.out.println("잘못된 입력입니다 새로시작하려면 1 종료하려면 2를 입력하세요.");
             }
         }
     }
