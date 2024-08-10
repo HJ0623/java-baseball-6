7df2ccd07203c2b152b080570bca85f6509377f3
hyeongjin <rlagudwls277@naver.com>
2024-08-10 21:20:48 +0900

    Refactor:기능 세부화

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -5,19 +5,29 @@ import java.util.Set;
 
 public class Validator {
     public static void validateInput(String input) {
-        if (input.length() != 3) {     // 길이가 3이 아닐떄
-            throw new IllegalArgumentException();
-        }
-        Set<Character> uniqueDigits = new HashSet<>();
-        for (char c : input.toCharArray()) { //각문자가 숫자인지 0인지 확인
-            if (!Character.isDigit(c) || c == '0') {
-                throw new IllegalArgumentException();
-            }
-            uniqueDigits.add(c);
-        }
-        if (uniqueDigits.size() != 3) {  // 고유한 숫자의 개수확인
-            throw new IllegalArgumentException();
-        }
-
+        validateLength(input);
+        validateNumber(input);
     }
-}
+
+    private static void validateLength(String input) {
+        if (input.length() != 3) {     // 길이가 3이 아닐떄
+            throw new IllegalArgumentException("3자리 숫자를 입력해야합니다");
+        }
+    }
+
+
+    private static void validateNumber(String input){
+        Set<Character> uniqueDigits = new HashSet<>();
+            for (char c : input.toCharArray()) { //각문자가 숫자인지 0인지 확인
+                if (!Character.isDigit(c) || c == '0') {
+                    throw new IllegalArgumentException("1~9사이의 숫자를 대입해야합니다");
+                }
+                uniqueDigits.add(c);
+            }
+            if (uniqueDigits.size() != 3) {  // 중복확인
+                throw new IllegalArgumentException("중복된 숫자를 입력하셨습니다");
+            }
+
+        }
+    }
+
